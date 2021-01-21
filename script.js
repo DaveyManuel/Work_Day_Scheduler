07b79eb190d8fb76544a97225ed46b00cf53437d
David Sanchez <davidmsanchez250@gmail.com>
2021-01-20 22:10:34 -0500

    added rows and columns to HTML. Added divs and textareas. In JS I added text to each hour column using JQUERY. Using moment.js I created a variable to grab the number of the current hour. Using if then statement I changed styling of textarea comparing number found in data attribute to current hour. If then statement changes styling based on if the hour is in the past, present, or future.

--- script.js
+++ script.js
@@ -3,20 +3,80 @@
 $( document ).ready(function() {
 
 const currentDay = moment().format('dddd, MMMM Do, YYYY');
-const timeBlockContainer = $(".container")
-const timeSlotDiv = $("<div>").attr("id", "newDiv")
+const currentHour = moment().hours();
 
-console.log(timeBlockContainer);
 
-console.log(timeSlotDiv);
+console.log(currentHour)
 
-console.log(currentDay);
+const timeBlockContainer = $(".container");
+
+$("#9AM").text("9AM").addClass("hour");
+$("#10AM").text("10AM").addClass("hour");
+$("#11AM").text("11AM").addClass("hour");
+$("#12PM").text("12PM").addClass("hour");
+$("#1PM").text("1PM").addClass("hour");
+$("#2PM").text("2PM").addClass("hour");
+$("#3PM").text("3PM").addClass("hour");
+$("#4PM").text("4PM").addClass("hour");
+$("#5PM").text("5PM").addClass("hour");
+
+
+
+let x = $("#userInput9").data("number")
+
+console.log(x);
+
+if (x<currentHour) {
+    
+    $("#userInput9").addClass("past")
+} else if (x=currentHour){
+
+    $("#userInput9").addClass("present")
+
+} else {
+
+    $("#userInput9").addClass("future")
+}
+
+
+
+// $(".timeBlock").each(function(){
+
+//     let dataHour = $(this).data("-number");
+
+//     console.log(dataHour);
+
+//     console.log($(".timeBlock"));
+   
+// });
+
+
+// const pastTimeSlotDiv = $("<textarea>").addClass("past row");
+// const presentTimeSlotDiv = $("<textarea>").addClass("present row");
+// const futureTimeSlotDiv = $("<textarea>").addClass("future row");
+
+// console.log(timeBlockContainer);
+
+// console.log(timeSlotDiv);
+
+// console.log(currentDay);
 
 $("#currentDay").text(currentDay)
 
-const timeSlotRow = timeBlockContainer.append(timeSlotDiv)
+ 
+// let r = timeBlockContainer.append(hour)
+//r.after(pastTimeSlotDiv)
+// timeBlockContainer.append(pastTimeSlotDiv)
+// timeBlockContainer.append(presentTimeSlotDiv)
+// timeBlockContainer.append(futureTimeSlotDiv)
 
-//timeSlotRow.addClass(".present")
+    
+
+
+
+//get style of divs to change with the hour
+//add save button
+//localStorage functionality for save button
 
 
 
