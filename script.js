e17e98a613610f7e49cb24577dc49435b8a1a530
David Sanchez <davidmsanchez250@gmail.com>
2021-01-20 23:03:17 -0500

    added if then statements to each userInput section so that it would change style dependent on time

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ $( document ).ready(function() {
 
 const currentDay = moment().format('dddd, MMMM Do, YYYY');
 const currentHour = moment().hours();
+const saveBtn = $(".col-2")
 
 
 console.log(currentHour)
 
-const timeBlockContainer = $(".container");
+$("#currentDay").text(currentDay);
+
+saveBtn.addClass("saveBtn");
+//add icon to saveBtn using .text?
 
 $("#9AM").text("9AM").addClass("hour");
 $("#10AM").text("10AM").addClass("hour");
@@ -22,14 +26,12 @@ $("#5PM").text("5PM").addClass("hour");
 
 
 
-let x = $("#userInput9").data("number")
+let userInput9 = $("#userInput9").data("number")
 
-console.log(x);
-
-if (x<currentHour) {
+if (userInput9<currentHour) {
     
     $("#userInput9").addClass("past")
-} else if (x=currentHour){
+} else if (userInput9=currentHour){
 
     $("#userInput9").addClass("present")
 
@@ -37,6 +39,120 @@ if (x<currentHour) {
 
     $("#userInput9").addClass("future")
 }
+
+let userInput10 = $("#userInput10").data("number")
+
+if (userInput10<currentHour) {
+    
+    $("#userInput10").addClass("past")
+} else if (userInput10=currentHour){
+
+    $("#userInput10").addClass("present")
+
+} else {
+
+    $("#userInput10").addClass("future")
+}
+
+let userInput11 = $("#userInput11").data("number")
+
+if (userInput11<currentHour) {
+    
+    $("#userInput11").addClass("past")
+} else if (userInput11=currentHour){
+
+    $("#userInput11").addClass("present")
+
+} else {
+
+    $("#userInput11").addClass("future")
+}
+
+let userInput12 = $("#userInput12").data("number")
+
+if (userInput12<currentHour) {
+    
+    $("#userInput12").addClass("past")
+} else if (userInput12=currentHour){
+
+    $("#userInput12").addClass("present")
+
+} else {
+
+    $("#userInput12").addClass("future")
+}
+
+let userInput13 = $("#userInput13").data("number")
+
+if (userInput13<currentHour) {
+    
+    $("#userInput13").addClass("past")
+} else if (userInput13=currentHour){
+
+    $("#userInput13").addClass("present")
+
+} else {
+
+    $("#userInput13").addClass("future")
+}
+
+let userInput14 = $("#userInput14").data("number")
+
+if (userInput14<currentHour) {
+    
+    $("#userInput14").addClass("past")
+} else if (userInput14=currentHour){
+
+    $("#userInput14").addClass("present")
+
+} else {
+
+    $("#userInput14").addClass("future")
+}
+
+let userInput15 = $("#userInput15").data("number")
+
+if (userInput15<currentHour) {
+    
+    $("#userInput15").addClass("past")
+} else if (userInput15=currentHour){
+
+    $("#userInput15").addClass("present")
+
+} else {
+
+    $("#userInput15").addClass("future")
+}
+
+let userInput16 = $("#userInput16").data("number")
+
+if (userInput16<currentHour) {
+    
+    $("#userInput16").addClass("past")
+} else if (userInput16=currentHour){
+
+    $("#userInput16").addClass("present")
+
+} else {
+
+    $("#userInput16").addClass("future")
+}
+
+let userInput17 = $("#userInput17").data("number")
+
+if (userInput17<currentHour) {
+    
+    $("#userInput17").addClass("past")
+} else if (userInput17=currentHour){
+
+    $("#userInput17").addClass("present")
+
+} else {
+
+    $("#userInput17").addClass("future")
+}
+
+
 
 
 
@@ -61,7 +177,7 @@ if (x<currentHour) {
 
 // console.log(currentDay);
 
-$("#currentDay").text(currentDay)
+
 
  
 // let r = timeBlockContainer.append(hour)
