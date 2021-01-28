3666eb58c7bc557ee350cb1ada7466b1c9d8dc7e
David Sanchez <davidmsanchez250@gmail.com>
2021-01-27 20:39:03 -0500

    refactored if then statements using the each function. Used same logic. Deleted uneccessary comments.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 $( document ).ready(function() {
 
 //Saved const variables 
@@ -15,6 +13,8 @@ const saveBtnIcon = $("<i>").addClass("fas fa-save saveBtn");
 
 $("#currentDay").text(currentDay);
 $("#currentTime").text(currentTime);
+
+//setInterval(change text every minute)
 
 //Add saveBtn class to Save Button, add icon to button
 
@@ -33,15 +33,7 @@ $("#3PM").text("3PM").addClass("hour");
 $("#4PM").text("4PM").addClass("hour");
 $("#5PM").text("5PM").addClass("hour");
 
-//dynamically add data attr to save buttons
-// $("#saveBtn9").attr("data-number", 9);
-// $("#saveBtn10").data("number", 10);
-
-//$("#userInput9").val(localStorage.getItem("userInput9"));
-// $("#userInput10").val(localStorage.getItem("userInput10"));
-// $("#userInput11").val(localStorage.getItem("userInput11"));
-// $("#userInput12").val(localStorage.getItem("userInput12"));
-
+//Below I used localStorage.getItem to get the value for each textarea and created a unique key for each hour
 
 $("#userInput9").val(localStorage.getItem('hour9'));
 $("#userInput10").val(localStorage.getItem('hour10'));
@@ -54,14 +46,10 @@ $("#userInput16").val(localStorage.getItem('hour16'));
 $("#userInput17").val(localStorage.getItem('hour17'));
 
 
-
-
-
 //Functionality for Save Button
 
 $(".saveBtn").on("click", function(){
  
-
     localStorage.setItem('hour9',$("#userInput9").val());
     localStorage.setItem('hour10',$("#userInput10").val());
     localStorage.setItem('hour11',$("#userInput11").val());
@@ -72,213 +60,22 @@ $(".saveBtn").on("click", function(){
     localStorage.setItem('hour16',$("#userInput16").val());
     localStorage.setItem('hour17',$("#userInput17").val());
 
-   
 })
 
-
-//Created variables for the data attribute of each textarea. Then I created 'if, then' statements to set class dependent on if the data number was less than, equal, or greater than the current hour of the day.  
-
-//dyanmically grab textarea and put in for if else statements. Loop through using for loop
-
+//Created variable for the data attribute of each textarea. Then I created 'if', 'else' statements to set class based on if the data attribute is less than, equal to, or greater than the currentHour variable.
 
 $(".textInput").each(function() {
 
     let x = $(this).attr("data-number")
-    console.log(x)
 
     if (x<currentHour) {
         $(this).addClass("past")
     } else if (x===currentHour){
         $(this).addClass("present")
-        $(this).removeClass("future")
-        $(this).removeClass("past")
     } else {
         $(this).addClass("future")
     }
 
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-// let userInput9 = $("#userInput9").data("number")
-
-// if (userInput9<currentHour) {
-    
-//     $("#userInput9").addClass("past")
-// } else if (userInput9===currentHour){
-
-//     $("#userInput9").addClass("present")
-
-// } else {
-
-//     $("#userInput9").addClass("future")
-// }
-
-// let userInput10 = $("#userInput10").data("number")
-
-// if (userInput10<currentHour) {
-    
-//     $("#userInput10").addClass("past")
-// } else if (userInput10===currentHour){
-
-//     $("#userInput10").addClass("present")
-
-// } else {
-
-//     $("#userInput10").addClass("future")
-// }
-
-// let userInput11 = $("#userInput11").data("number")
-
-// if (userInput11<currentHour) {
-    
-//     $("#userInput11").addClass("past")
-// } else if (userInput11===currentHour){
-
-//     $("#userInput11").addClass("present")
-
-// } else {
-
-//     $("#userInput11").addClass("future")
-// }
-
-// let userInput12 = $("#userInput12").data("number")
-
-// if (userInput12<currentHour) {
-    
-//     $("#userInput12").addClass("past")
-// } else if (userInput12===currentHour){
-
-//     $("#userInput12").addClass("present")
-
-// } else {
-
-//     $("#userInput12").addClass("future")
-// }
-
-// let userInput13 = $("#userInput13").data("number")
-
-// if (userInput13<currentHour) {
-    
-//     $("#userInput13").addClass("past")
-// } else if (userInput13===currentHour){
-
-//     $("#userInput13").addClass("present")
-
-// } else {
-
-//     $("#userInput13").addClass("future")
-// }
-
-// let userInput14 = $("#userInput14").data("number")
-
-// if (userInput14<currentHour) {
-    
-//     $("#userInput14").addClass("past")
-// } else if (userInput14===currentHour){
-
-//     $("#userInput14").addClass("present")
-
-// } else {
-
-//     $("#userInput14").addClass("future")
-// }
-
-// let userInput15 = $("#userInput15").data("number")
-
-// if (userInput15<currentHour) {
-    
-//     $("#userInput15").addClass("past")
-// } else if (userInput15===currentHour){
-
-//     $("#userInput15").addClass("present")
-
-// } else {
-
-//     $("#userInput15").addClass("future")
-// }
-
-// let userInput16 = $("#userInput16").data("number")
-
-// if (userInput16<currentHour) {
-    
-//     $("#userInput16").addClass("past")
-// } else if (userInput16===currentHour){
-
-//     $("#userInput16").addClass("present")
-
-// } else {
-
-//     $("#userInput16").addClass("future")
-// }
-
-// let userInput17 = $("#userInput17").data("number")
-
-// if (userInput17<currentHour) {
-    
-//     $("#userInput17").addClass("past")
-// } else if (userInput17===currentHour){
-
-//     $("#userInput17").addClass("present")
-
-// } else {
-
-//     $("#userInput17").addClass("future")
-// }
-
-
-
-
-
-// $(".timeBlock").each(function(){
-
-//     let dataHour = $(this).data("-number");
-
-//     console.log(dataHour);
-
-//     console.log($(".timeBlock"));
-   
-// });
-
-
-// const pastTimeSlotDiv = $("<textarea>").addClass("past row");
-// const presentTimeSlotDiv = $("<textarea>").addClass("present row");
-// const futureTimeSlotDiv = $("<textarea>").addClass("future row");
-
-// console.log(timeBlockContainer);
-
-// console.log(timeSlotDiv);
-
-// console.log(currentDay);
-
-
-
- 
-// let r = timeBlockContainer.append(hour)
-//r.after(pastTimeSlotDiv)
-// timeBlockContainer.append(pastTimeSlotDiv)
-// timeBlockContainer.append(presentTimeSlotDiv)
-// timeBlockContainer.append(futureTimeSlotDiv)
-
-    
-
-
-
-//add save button
-//localStorage functionality for save button
-
 });
-
-
-
-
